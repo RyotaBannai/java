bf31ba9887b5f287bf302992120eedfed51551fc
RyotaBannai <ryotala0528@gmail.com>
2020-08-30 16:06:36 +0900

    add: read java just before how to use array.

--- archive/basics/hello.java
+++ archive/basics/hello.java
@@ -1,5 +1,34 @@
+
+/**
+ * javadoc
+ */
+
 class Hello {
   public static void main(String[] args) {
-    System.out.println("hello");
+    String hello = "hello";
+    String message = hello;
+    var this_is_string_type = "string";
+    int data[] = { 85, 72, 89 };
+
+    // for (int i = 0; i < data.length; i++){ System.out.println(data[i]); }
+    // previous way
+    for (int seiseki : data) {
+      System.out.println(seiseki);
+    }
+    System.out.println(this_is_string_type);
+  }
+}
+
+class Calc {
+  final float PI = 3.1415F; // 定数として使いたい
+  int result;
+  float float_result;
+
+  void plus(int val1, int val2) {
+    result = val1 + val2;
+  }
+
+  void calcCircumstance(int val1) {
+    float_result = val1 * PI;
   }
 }
