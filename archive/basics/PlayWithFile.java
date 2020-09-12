db74d609bf2f7db2ce3737375d13a9a02c454d71
RyotaBannai <ryotala0528@gmail.com>
2020-09-12 23:26:08 +0900

    add: started to learn design pattern in java

--- archive/basics/PlayWithFile.java
+++ archive/basics/PlayWithFile.java
@@ -2,7 +2,7 @@ import java.io.File;
 
 public class PlayWithFile {
   public static void main(String args[]) {
-    File dir = new File("./"); // 相対パスだとファイルの一ではなく、実行する位置
+    File dir = new File("./"); // 相対パスだとファイル位置ではなく実行する位置
     System.out.println(dir.isDirectory());
 
     File filelist[] = dir.listFiles();
