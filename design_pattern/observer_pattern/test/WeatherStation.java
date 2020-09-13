4f0027cb22acabfa42481b48945b66b04a5eeefe
RyotaBannai <ryotala0528@gmail.com>
2020-09-13 18:18:25 +0900

    add: heat index display

--- design_pattern/observer_pattern/test/WeatherStation.java
+++ design_pattern/observer_pattern/test/WeatherStation.java
@@ -1,14 +1,15 @@
 package test;
 
-import observer.CurrentConditionsDisplay;
+import observer.*;
 import subject.WeatherData;
 
 public class WeatherStation {
   public static void main(String args[]) {
     WeatherData weatherData = new WeatherData();
     CurrentConditionsDisplay currentDisplay = new CurrentConditionsDisplay(weatherData);
-    // StatisticsDisplay statisticsDisplay = new StatisticsDisplay(weatherData);
+    HeatIndexDisplay heatIndexDisplay = new HeatIndexDisplay(weatherData);
 
     weatherData.setMeasurements(27f, 65f, 30.4f);
+
   }
 }
