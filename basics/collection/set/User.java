5b9a1507d42260dfaa75e4566ba6507fc88b8f19
RyotaBannai <ryotala0528@gmail.com>
2020-09-25 23:47:25 +0900

    add: started to learn java collection

---
+++ basics/collection/set/User.java
@@ -0,0 +1,34 @@
+    class User {
+        private int id;
+        private String name;
+
+        public User(int id, String name) {
+            this.id = id;
+            this.name = name;
+        }
+
+        public int hashCode() {
+            return this.id * 13;
+        }
+
+        public boolean equals(Object other) {
+
+            if (this == other) {
+                return true;
+            }
+
+            if (!(other instanceof User)) {
+                return false;              
+            }
+
+            User otherUser = (User) other;
+            if (this.id == otherUser.getId()) { 
+                return true;
+            }
+            return false;
+        }
+
+        public String toString() {
+            return "User : id = " + id + " name = " + name;
+        }
+    }
