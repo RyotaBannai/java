d8bdd86b128629f07146386882a2b1516883248b
RyotaBannai <ryotala0528@gmail.com>
2020-09-26 02:01:45 +0900

    add: started to learn java collection

--- basics/collection/set/User.java
+++ basics/collection/set/User.java
@@ -1,34 +1,34 @@
-    class User {
-        private int id;
-        private String name;
+class User {
+    private int id;
+    private String name;
 
-        public User(int id, String name) {
-            this.id = id;
-            this.name = name;
+    public User(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int hashCode() {
+        return this.id * 13;
+    }
+
+    public boolean equals(Object other) {
+
+        if (this == other) {
+            return true;
         }
 
-        public int hashCode() {
-            return this.id * 13;
-        }
-
-        public boolean equals(Object other) {
-
-            if (this == other) {
-                return true;
-            }
-
-            if (!(other instanceof User)) {
-                return false;              
-            }
-
-            User otherUser = (User) other;
-            if (this.id == otherUser.getId()) { 
-                return true;
-            }
+        if (!(other instanceof User)) {
             return false;
         }
 
-        public String toString() {
-            return "User : id = " + id + " name = " + name;
+        User otherUser = (User) other;
+        if (this.id == otherUser.getId()) {
+            return true;
         }
+        return false;
     }
+
+    public String toString() {
+        return "User : id = " + id + " name = " + name;
+    }
+}
