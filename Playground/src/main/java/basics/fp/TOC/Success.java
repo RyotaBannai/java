ea1b55d065494013af14f8317470bdd260fbc33f
RyotaBannai <ryotala0528@gmail.com>
2020-11-01 21:01:36 +0900

    add: apply BigInteger to factorial

--- Playground/src/main/java/basics/fp/TOC/Success.java
+++ Playground/src/main/java/basics/fp/TOC/Success.java
@@ -1,22 +1,31 @@
 package basics.fp.TOC;
 
+import java.math.BigInteger;
 import java.util.stream.Stream;
 
+/**
+ * Example - Factorial
+ * http://www.fredosaurus.com/notes-java/data/numbers/60factorial.html
+ */
 public class Success {
     public static void main(String[] args) {
         System.out.println(
-                String.format("the result is %d",
-                        factorialTailRec(1, 15).invoke()));
+                String.format("the result is %d", factorial(BigInteger.valueOf(21))));
     }
 
-    private static TailCall<Integer> factorialTailRec(
-            final int factorial,
-            final int number) {
-        if (number == 1) {
+    // 0 を渡すとエラーになる・invoke を呼ぶ必要がある -> 詳細は隠蔽
+    public static BigInteger factorial(final BigInteger number) {
+        return factorialTailRec(BigInteger.ONE, number).invoke();
+    }
+
+    private static TailCall<BigInteger> factorialTailRec(
+            final BigInteger factorial,
+            final BigInteger number) {
+        if (number.equals(BigInteger.ONE)) {
             return TailCalls.done(factorial);
         } else {
             return TailCalls.call(
-                    () -> factorialTailRec(factorial * number, number - 1));
+                    () -> factorialTailRec(BigIntegerMethods.multiply(factorial, number), BigIntegerMethods.decrement(number)));
         }
     }
 
@@ -32,8 +41,10 @@ public class Success {
             throw new Error("not implemented");
         }
 
+        // 最初に一度だけ呼ぶ
         default T invoke() {
             return Stream.iterate(this, TailCall::apply)
+//                    .peek(current -> System.out.println(current.isComplete()))
                     .filter(TailCall::isComplete)
                     .findFirst()
                     .get()
@@ -63,6 +74,17 @@ public class Success {
                     throw new Error("not implemented;");
                 }
             };
+        }
+    }
+
+    public static class BigIntegerMethods {
+        public static BigInteger decrement(final BigInteger number) {
+            return number.subtract(BigInteger.ONE);
+        }
+
+        public static BigInteger multiply(
+                final BigInteger first, final BigInteger second) {
+            return first.multiply(second);
         }
     }
 }
