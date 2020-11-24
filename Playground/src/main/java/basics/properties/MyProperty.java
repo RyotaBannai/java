a3bf5a1d1e52eca6198436790b81f28507ce1449
RyotaBannai <ryotala0528@gmail.com>
2020-11-24 23:51:34 +0900

    add: xml property, system property, and pass property when it started.

--- Playground/src/main/java/basics/properties/MyProperty.java
+++ Playground/src/main/java/basics/properties/MyProperty.java
@@ -1,25 +1,60 @@
 package basics.properties;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Properties;
 
+
+/**
+ * システムプロパティ値を起動オプションを使って渡す -D<キー名>="値"
+ * java -jar -Dxyz="foo" TestApp3.jar
+ */
+
 public class MyProperty {
-    public static void main(String[] args) {
-        try {
-            test();
-        } catch (IOException error) {
-            error.printStackTrace();
-        }
+    public static void main(String[] argas) {
+        Properties properties = System.getProperties();
+        System.out.println(properties.getProperty("user.home"));
+        properties.list(System.out);
+
+//        try {
+//            read();
+//        } catch (IOException error) {
+//            error.printStackTrace();
+//        }
     }
 
-    private static void test() throws IOException {
+    /**
+     * Properties は java.util.Hashtable から派生して、キー・バリューペアを実現
+     * プロパティ値は String 型のみ
+     * //
+     * foo,bar,baz: メタ構文変数 (metasyntactic variable)  (RFC 3092)
+     */
+
+    private static void write() throws IOException {
         Properties settings = new Properties();
         settings.setProperty("country", "USA");
         settings.setProperty("language", "English");
 
-        try (FileOutputStream outputStream = new FileOutputStream("basics.properties.myProperties")) {
+        File outputDir = new File("src/main/java/basics/properties/");
+        File outputFile = new File(outputDir, "myProperties");
+
+        try (FileOutputStream outputStream = new FileOutputStream(outputFile)) {
             settings.store(outputStream, "Test Properties");
         }
+    }
+
+    private static void read() throws IOException {
+        Properties settings = new Properties();
+
+        File inputDir = new File("src/main/java/basics/properties/");
+        File inputFile = new File(inputDir, "myProperties");
+
+        try (FileInputStream inputStream = new FileInputStream(inputFile)) {
+            settings.load(inputStream);
+        }
+        System.out.println(settings.getProperty("country"));
+        System.out.println(settings.getProperty("language"));
     }
 }
--- Playground/src/main/java/basics/properties/MyPropertyXML.java
+++ Playground/src/main/java/basics/properties/MyPropertyXML.java
@@ -1,4 +1,48 @@
 package basics.properties;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+/**
+ * 設定値を他のシステムから読み出す必要がある場合は XML 形式で保存しておくと、
+ * XML のパーサーが利用できるので便利なことがあり
+ */
 public class MyPropertyXML {
+    public static void main(String[] args) {
+        try {
+            readXML();
+        } catch (IOException error) {
+            error.printStackTrace();
+        }
+    }
+
+    private static void writeXML() throws IOException {
+        Properties settings = new Properties();
+        settings.setProperty("country", "USA");
+        settings.setProperty("language", "English");
+
+        File outputDir = new File("src/main/java/basics/properties/");
+        File outputFile = new File(outputDir, "myPropertiesXML");
+
+        try (FileOutputStream outputStream = new FileOutputStream(outputFile)) {
+            settings.storeToXML(outputStream, "Test Properties");
+        }
+    }
+
+
+    private static void readXML() throws IOException {
+        Properties settings = new Properties();
+
+        File inputDir = new File("src/main/java/basics/properties/");
+        File inputFile = new File(inputDir, "myPropertiesXML");
+
+        try (FileInputStream inputStream = new FileInputStream(inputFile)) {
+            settings.loadFromXML(inputStream);
+        }
+        System.out.println(settings.getProperty("country"));
+        System.out.println(settings.getProperty("language"));
+    }
 }
